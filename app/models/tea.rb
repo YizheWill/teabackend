14c797a93c9f11e52d7826f8dc064100521f2407
Yizhe Wang <yizhe87@gmail.com>
2020-11-30 11:27:44 -0800

    deleteall

--- app/models/tea.rb
+++
@@ -1,4 +0,0 @@
-class Tea < ApplicationRecord
-  validates :flavor, :amount, presence: true
-  validates :flavor, uniqueness: { case_sensitive: false }
-end
--- frontend/reducers/selectors.js
+++
@@ -1,5 +0,0 @@
-// used to filter/select specific things from redux state
-
-export const selectAllTeas = (state) => {
-    return Object.values(state.teas);
-}
